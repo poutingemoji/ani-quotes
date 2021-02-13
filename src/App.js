5afa776224e6411398d3727969a38e0a80a6060a
poutingemoji <9awu680@gmail.com>
2021-02-12 23:55:28 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './images/logo.svg';
 import './App.css';
 
 function App() {
@@ -6,7 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p class="text-blue-400">
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
