ffa5662eac11e1728a770500249bed839702efae
poutingemoji <9awu680@gmail.com>
2021-03-04 05:42:54 -0800

    removed authors and topics, added pagination

--- src/pages/search.js
+++ src/pages/search.js
@@ -5,24 +5,33 @@ import ImageButton from "../components/ImageButton";
 import { Link } from "react-router-dom";
 import authorQuotes from "../data/authorQuotes";
 import { groupBy } from "../utils/Helper";
+import Masonry from "../components/Masonry";
+import ReactPaginate from "react-paginate";
+
+const PER_PAGE = 18;
 
 function Search({ quotes, authors }) {
+  const [currentPage, setCurrentPage] = useState(0);
+  const offset = currentPage * PER_PAGE;
   const [query, updateQuery] = useState("");
   function handleKeyDown({ key, currentTarget }) {
     if (!(key === "Enter")) return;
     updateQuery(currentTarget.value);
+    setCurrentPage(0);
   }
-  const topicQuotes = groupBy(quotes, (quote) => quote.topics);
-  const quoteResults = quotes.filter((quote) =>
-    quote.text.toLowerCase().includes(query.toLowerCase())
+
+  function handlePageChange({ selected }) {
+    setCurrentPage(selected);
+  }
+
+  const quoteResults = quotes.filter(
+    (quote) =>
+      quote.text.toLowerCase().includes(query.toLowerCase()) ||
+      quote.topics.includes(query.replace(/ /g, "_")) ||
+      quote.author.name.full.toLowerCase().includes(query.toLowerCase())
   );
-  const authorResults = authors.filter((author) =>
-    author.name.full.toLowerCase().includes(query.toLowerCase())
-  );
-  const topicResults = Object.keys(topics).filter((key) =>
-    key.replace(/_/g, " ").includes(query.toLowerCase())
-  );
-  console.log(topicResults);
+
+  const pageCount = Math.ceil(quoteResults.length / PER_PAGE);
   return (
     <div className="flex flex-col items-center pt-20">
       <input
@@ -30,51 +39,34 @@ function Search({ quotes, authors }) {
         type="text"
         onKeyDown={handleKeyDown}
       />
-      <div className="flex flex-row justify-center">
+      <div className="flex flex-col justify-center">
         {query.length > 0 && (
-          <>
-            <div className="flex flex-col w-1/3">
-              {quoteResults.length > 0 &&
-                quoteResults
-                  .slice(0, 10)
-                  .map((quote, i) => (
-                    <Card
-                      key={i}
-                      author={quote.author}
-                      text={quote.text}
-                      image={quote.image}
-                      tags={quote.topics}
-                    />
-                  ))}
-            </div>
-            <div className="flex flex-col w-1/6">
-              {authorResults.length > 0 &&
-                authorResults.slice(0, 10).map((author, i) => (
-                  <Link to={`/authors/${author.id}`} key={i}>
-                    <ImageButton
-                      className={"h-96"}
-                      title={author.name.full}
-                      numOfQuotes={authorQuotes[author.id].length}
-                      image={author.image.large}
-                    />
-                  </Link>
+          <Masonry>
+            {quoteResults.length > 0 &&
+              quoteResults
+                .slice(offset, offset + PER_PAGE)
+                .map((quote, i) => (
+                  <Card
+                    key={i}
+                    author={quote.author}
+                    text={quote.text}
+                    image={quote.image}
+                    tags={quote.topics}
+                  />
                 ))}
-            </div>
-            <div className="flex flex-col w-1/6">
-              {topicResults.length > 0 &&
-                topicResults.slice(0, 10).map((key, i) => (
-                  <Link to={`/topics/${key}`} key={i}>
-                    <ImageButton
-                      className={"h-80"}
-                      icon={topics[key]?.icon}
-                      title={key}
-                      numOfQuotes={1}
-                      image={topics[key]?.image}
-                    />
-                  </Link>
-                ))}
-            </div>{" "}
-          </>
+          </Masonry>
+        )}
+        {pageCount > 1 && (
+          <ReactPaginate
+            previousLabel={"«"}
+            nextLabel={"»"}
+            pageCount={pageCount}
+            pageRangeDisplayed={10}
+            onPageChange={handlePageChange}
+            containerClassName={"pagination"}
+            activeClassName={"active"}
+            forcePage={currentPage}
+          />
         )}
       </div>
     </div>
